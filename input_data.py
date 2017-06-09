955eb718183c7c40011d1bd42e473c0cf2fb4f0e
shohei-ojs <shohei.ojs@gmail.com>
2017-06-09 04:20:05 +0000

    改良版をコミット

--- input_data.py
+++ input_data.py
@@ -8,6 +8,7 @@ class InputData(object):
 
   def __init__(self, data_dir='./data/', batch_size=64):
     self.train_data = self.__create_batchs(data_dir)
+    self.__test_data, self.__test_target = self.__create_test_data(data_dir)
     self.batch_size = batch_size
     self.idx = 0
   
@@ -22,15 +23,27 @@ class InputData(object):
     return trains, actuals
   
   
-  def __create_batchs(self, data_dir):
-    osaka_avgs, osaka_maxs, osaka_mins = self.__read_data(data_dir + 'osaka.csv')
-    tokyo_avgs, tokyo_maxs, tokyo_mins = self.__read_data(data_dir + 'tokyo.csv')
-    train_data = []
+  def __create_batchs(self, data_dir, test=False):
+    if not test:
+      osaka_avgs, osaka_maxs, osaka_mins = self.__read_data(data_dir + 'osaka.csv')
+      tokyo_avgs, tokyo_maxs, tokyo_mins = self.__read_data(data_dir + 'tokyo.csv')
+    else:
+      osaka_avgs, osaka_maxs, osaka_mins = self.__read_data(data_dir + 'test_osaka.csv')
+      tokyo_avgs, tokyo_maxs, tokyo_mins = self.__read_data(data_dir + 'test_tokyo.csv')
+    data = []
     for i in range(len(osaka_avgs) - 8):
-      train_data.append(([osaka_avgs[i:i+7], osaka_maxs[i:i+7], osaka_mins[i:i+7]], [tokyo_avgs[i+8], tokyo_maxs[i+8], tokyo_mins[i+8]]))
-    return train_data
-
+      data.append(([osaka_avgs[i:i+7] + tokyo_avgs[i+6:i+8], osaka_maxs[i:i+7] + tokyo_maxs[i+6:i+8], osaka_mins[i:i+7] + tokyo_mins[i+6:i+8]]
+          , [tokyo_avgs[i+8], tokyo_maxs[i+8], tokyo_mins[i+8]]))
+    return data
   
+  
+  def __create_test_data(self, data_dir):
+    test_data = self.__create_batchs(data_dir, test=True)
+    actuals = [actual for (train, actual) in test_data]
+    _input = [train for (train, actual) in test_data]
+    return _input, actuals
+
+
   def __read_data(self, data_path):
     avgs = []
     maxs = []
@@ -46,3 +59,6 @@ class InputData(object):
       reader = csv.reader(f)
       for row in reader:
         yield float(row[0]), float(row[1]), float(row[2])
+  
+  def test_data(self):
+    return self.__test_data, self.__test_target
--- main.py
+++ main.py
@@ -1,35 +1,65 @@
 import tensorflow as tf
-
+import random
 import input_data
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 
 
 TRAIN_SIZE = 7
 ACTUAL_SIZE = 3
-MAX_STEP = 10000
+MAX_STEP = 3000
 
 
 def train(loss):
   return tf.train.AdamOptimizer().minimize(loss)
 
-def predicrt(x):
-  print(x)
+
+def predicrt(x, rate):
   output = tf.contrib.layers.flatten(x)
-  output = tf.layers.dense(output, 50)
+  output = tf.layers.dense(output, 128)
   output = tf.nn.relu(output)
-  output = tf.layers.dense(output, 10)
+  output = tf.layers.dropout(output, rate=rate)
+  output = tf.layers.dense(output, 32)
   output = tf.nn.relu(output)
+  output = tf.layers.dropout(output, rate=rate)
+  output = tf.layers.dense(output, 16)
+  output = tf.nn.relu(output)
+  output = tf.layers.dropout(output, rate=rate)
   output = tf.layers.dense(output, 3)
   return output
 
 def loss(predict, actual):
-  return tf.reduce_mean(tf.pow(predict - actual, 2))
+  return l2loss(tf.reduce_mean(tf.pow(predict - actual, 2)))
+
+def l2loss(loss):
+  variables = tf.trainable_variables()
+  for v in variables:
+    if 'kernel' in v.name:
+      loss += tf.nn.l2_loss(v)
+  return loss
+
+def generate_graph(actual, predict, name):
+  fig = plt.figure()
+
+  ax = fig.add_subplot(1,1,1)
+
+  ax.scatter(actual, predict)
+
+  ax.set_title(name)
+  ax.set_xlabel('actual')
+  ax.set_ylabel('predict')
+
+  plt.savefig('./output/%s.png' % name)
+
 
 def main():
   data = input_data.InputData()
 
-  x = tf.placeholder(tf.float32, shape=[None, 3, 7])
+  x = tf.placeholder(tf.float32, shape=[None, 3, 9])
   y = tf.placeholder(tf.float32, shape=[None, 3])
-  predict_op = predicrt(x)
+  rate = tf.placeholder(tf.float32, shape=[])
+  predict_op = predicrt(x, rate)
   loss_op = loss(predict_op, y)
   train_op = train(loss_op)
   init_op = tf.global_variables_initializer()
@@ -37,8 +67,30 @@ def main():
     sess.run(init_op)
     for i in range(MAX_STEP):
       train_data, actual_data = data.next_batch()
-      _, _predict = sess.run([train_op, predict_op], feed_dict={x: train_data, y: actual_data})
-      #print(_predict[0], actual_data[0])
+      _, _predict, _loss = sess.run([train_op, predict_op, loss_op], feed_dict={x: train_data, y: actual_data, rate: 0.5})
+      if i % 100 == 0:
+        print('step: %05d loss: %0.05f' % (i, _loss))
+      if i % 1000 == 0:
+        test_data, test_target = data.test_data()
+        _loss, _predict = sess.run([loss_op, predict_op], feed_dict={x: test_data, y: test_target, rate: 1.0})
+        sample_predict_index = random.choice(range(len(_predict)))
+        sample_predict = _predict[sample_predict_index]
+        actual_tmp = test_target[sample_predict_index] 
+        print('Test loss: %00.05f sample_avg_tmp: %00.02f sample_max_tmp: %00.02f sample_min_tmp: %00.02f actual_avg_tmp: %00.02f actual_max_tmp: %00.02f actual_min_tmp: %00.02f'
+           % (_loss, sample_predict[0], sample_predict[1], sample_predict[2], actual_tmp[0], actual_tmp[1], actual_tmp[2]))
+    
+    _predicts = sess.run(predict_op, feed_dict={x: test_data, y: test_target, rate: 1.0})
+    actual_avg_tmps = [d[0] for d in test_target]
+    actual_max_tmps = [d[1] for d in test_target]
+    actual_min_tmps = [d[2] for d in test_target]
+    predict_avg_tmps = [p[0] for p in _predicts]
+    predict_max_tmps = [p[1] for p in _predicts]
+    predict_min_tmps = [p[2] for p in _predicts]
+    generate_graph(actual_avg_tmps, predict_avg_tmps, 'average')
+    generate_graph(actual_max_tmps, predict_max_tmps, 'max')
+    generate_graph(actual_min_tmps, predict_min_tmps, 'min')
+    # _predict = sess.run(predict_op, feed_dict={x:[[[20.5, 19.4, 20.3, 20.5, 21.4, 19.2, 21.2, 20.3, 22.0], [25.9, 24.5, 26.1, 27.0, 26.4, 22.6, 26.3, 22.6, 25.8], [16.9, 15.5, 15.7, 15.1, 16.2, 17.6, 17.9, 17.9, 19.2]]], rate: 1.0})
+    # print(_predict)
 
 if __name__ == '__main__':
   main()
